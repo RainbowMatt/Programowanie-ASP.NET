9825912fef12a11de2e45129a54496072405b963
Mateusz Witulski <witulskimateusz@outlook.com>
2023-11-24 17:20:06 +0100

    lab 3 zadanie 4

--- Programowanie ASP.NET/Controllers/TravelController.cs
+++ Programowanie ASP.NET/Controllers/TravelController.cs
@@ -5,10 +5,12 @@ namespace Programowanie_ASP.NET.Controllers
 {
     public class TravelController : Controller
     {
+        static Dictionary<int, Travel> _travels = new Dictionary<int, Travel>();
         public IActionResult Index()
         {
-            return View();
+            return View(_travels);
         }
+
         public IActionResult Create()
         {
             return View();
@@ -28,6 +30,74 @@ namespace Programowanie_ASP.NET.Controllers
 
             // Jeśli ModelState.IsValid nie jest spełnione, powracamy do widoku z formularzem i błędami
             return View(model);
-        }   
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_travels.ContainsKey(id))
+            {
+                return View(_travels[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Travel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_travels.ContainsKey(model.Id))
+                {
+                    _travels[model.Id] = model;
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_travels.ContainsKey(id))
+            {
+                return View(_travels[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Travel model)
+        {
+            if (_travels.ContainsKey(model.Id))
+            {
+                _travels.Remove(model.Id);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_travels.ContainsKey(id))
+            {
+                return View(_travels[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
+
