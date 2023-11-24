d0c1792780dd09c26ecaf256b932e2e791df8297
Mateusz Witulski <witulskimateusz@outlook.com>
2023-11-24 14:31:51 +0100

    lab 3 zadanie 1

---
+++ Programowanie ASP.NET/Models/Travel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Programowanie_ASP.NET.Models
+{
+    public class Travel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Proszę podać nazwe!")]
+        public string Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        public string StartingLocation { get; set; }
+        public string EndingLocation { get; set; }
+        public List<string> Participants { get; set; }
+        public string Guide { get; set; }
+    }
+}
