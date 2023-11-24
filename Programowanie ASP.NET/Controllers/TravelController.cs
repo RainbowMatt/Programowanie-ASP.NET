552b17b5ca090b9a57b270ddc4393f0f71bd72df
Mateusz Witulski <witulskimateusz@outlook.com>
2023-11-24 14:33:50 +0100

    lab3 zadanie 2

---
+++ Programowanie ASP.NET/Controllers/TravelController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Programowanie_ASP.NET.Models;
+
+namespace Programowanie_ASP.NET.Controllers
+{
+    public class TravelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Travel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Tutaj możesz umieścić kod do zapisu podróży do bazy danych lub innego rodzaju przechowywania danych
+                // np. SaveTravelToDatabase(model);
+
+                // Po zapisie możesz przekierować do widoku z listą podróży lub innych akcji
+                // return RedirectToAction("Index");
+            }
+
+            // Jeśli ModelState.IsValid nie jest spełnione, powracamy do widoku z formularzem i błędami
+            return View(model);
+        }
+    }
+}
