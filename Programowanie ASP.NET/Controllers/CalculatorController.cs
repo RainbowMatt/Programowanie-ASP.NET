628fa21921a8ef0507251a3a82670e93d731bd6a
Mateusz Witulski <witulskimateusz@outlook.com>
2023-11-24 13:28:20 +0100

    lab 2 zadanie 1

---
+++ Programowanie ASP.NET/Controllers/CalculatorController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Programowanie_ASP.NET.Models;
+using static Programowanie_ASP.NET.Controllers.HomeController;
+
+namespace Programowanie_ASP.NET.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Result(Calculator model)
+        {
+            if (!model.IsValid())
+            {
+                return View("Error");
+            }
+            return View(model);
+        }
+        /*public enum Operator
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+        public IActionResult Result(Operator op, double? a, double? b)
+        {
+            double result = 0;
+            if (a == null || b == null || op == Operator.Unknown)
+            {
+                ViewBag.Op = Operator.Unknown;
+                ViewBag.A = null;
+                ViewBag.B = null;
+                return View();
+            }
+
+            switch (op)
+            {
+                case Operator.Add:
+                    result = a.Value + b.Value;
+                    break;
+                case Operator.Sub:
+                    result = a.Value - b.Value;
+                    break;
+                case Operator.Mul:
+                    result = a.Value * b.Value;
+                    break;
+                case Operator.Div:
+                    if (b.Value != 0)
+                    {
+                        result = a.Value / b.Value;
+                    }
+                    else
+                    {
+                        b = null;
+                    }
+                    break;
+            }
+            ViewBag.Op = op;
+            ViewBag.A = a;
+            ViewBag.B = b;
+            ViewBag.Result = result;
+            return View();
+        }*/
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
---
+++ Programowanie ASP.NET/Models/Calculator .cs
@@ -0,0 +1,60 @@
+﻿namespace Programowanie_ASP.NET.Models
+{
+    public class Calculator
+    {
+        public Operators? Operator { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+
+        public string Op
+        {
+            get
+            {
+                switch (Operator)
+                {
+                    case Operators.Add:
+                        return "+";
+                    case Operators.Subtract:
+                        return "-";
+                    case Operators.Multiply:
+                        return "*";
+                    case Operators.Divide:
+                        return "/";
+                    default:
+                        return "";
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            return Operator != null && X != null && Y != null;
+        }
+
+        public double Calculate()
+        {
+            switch (Operator)
+            {
+                case Operators.Add:
+                    return (double)(X + Y);
+                case Operators.Subtract:
+                    return (double)(X - Y);
+                case Operators.Multiply:
+                    return (double)(X * Y);
+                case Operators.Divide:
+                    return Y != 0 ? (double)(X / Y) : double.NaN;
+                default:
+                    return double.NaN;
+            }
+        }
+    }
+
+    public enum Operators
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
+}
